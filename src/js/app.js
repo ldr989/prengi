4f9726c51bf72080017d127f322db2b891478331
Ldr <89.freedom@gmail.com>
2022-06-14 11:26:57 +0600

    added header, button and slider constructions

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+// configure Swiper to use modules
+modules: [Navigation, Pagination],
+});

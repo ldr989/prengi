c46d8740a67e4f0dcab1a93ece1d7c3734223736
Ldr <89.freedom@gmail.com>
2022-06-16 13:58:46 +0600

    added slider changes

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,6 @@ flsFunctions.isWebp();
 // core version + navigation, pagination modules:
 import Swiper, { Navigation, Pagination } from 'swiper';
 // import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 // init Swiper:
 const swiper = new Swiper('.swiper', {

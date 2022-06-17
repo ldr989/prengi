3844b63fc1fdd244c50e4f5c8e342e66e781bf17
Ldr <89.freedom@gmail.com>
2022-06-17 11:07:53 +0600

    added some slider changes

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,25 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper from 'swiper/bundle';
 // import Swiper and modules styles
 
 // init Swiper:
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.image-slider', {
 // configure Swiper to use modules
-modules: [Navigation, Pagination],
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
 });
